5086febf2e02cd7267a3e2c100f69eeed684c32b
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-03-02 18:10:51 +0300

    add pricessing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from pickle import FALSE
+
 from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 examples_all = [
@@ -22,13 +25,26 @@ examples_cards = [
 
 examples_accaunt = ["64686473678894779589", "35383033474447895560", "73654108430135874305"]
 
+exemple_processing = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+print("список маски номерка карты и номера счета:\n")
 for example in examples_all:
     print(mask_account_card(example))
 
+print("список маски номерка карты:\n")
 for example_card in examples_cards:
     print(get_mask_card_number(example_card))
 
+print("список маски номерка счета:\n")
 for example_accaunt in examples_accaunt:
     print(get_mask_account(example_accaunt))
 
 print(get_date("2024-03-11T02:26:18.671407"))
+print(f"отфильтрованный список:\n{filter_by_state(exemple_processing)}\n")
+print(f"отсортрованный список по убыванию:\n{sort_by_date(exemple_processing)}\n")
+print(f"отсортрованный список по возростанию:\n{sort_by_date(exemple_processing, False)}\n")
---
+++ src/processing.py
@@ -0,0 +1,17 @@
+from src.widget import get_date
+
+
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Фильтрует список словарей по значению ключа 'state'"""
+    filtered_by_state = []
+    for item in data:
+        if item.get("state") == state:
+            filtered_by_state.append(item)
+    return filtered_by_state
+
+
+def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
+    """Сортирует список словарей по значению ключа 'data'"""
+    sorted_data = data[:]
+    sorted_data.sort(key=lambda x: get_date(x["date"]), reverse=descending)
+    return sorted_data
