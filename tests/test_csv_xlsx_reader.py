47366d5b47e773556208449ea865701eb2e621e7
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-03-30 16:49:45 +0300

    add test for reading xlsx file

--- tests/test_csv_xlsx_reader.py
+++ tests/test_csv_xlsx_reader.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch, mock_open
 
-from csv_xlsx_reader import csv_reader
+import pandas as pd
+
+from csv_xlsx_reader import csv_reader, exel_reader
 
 
 @patch("builtins.open", new_callable=mock_open,
@@ -17,3 +19,17 @@ def test_csv_reader(mock_dict_reader, mock_file):
     assert result[0]["date"] == "2024-01-01"
     assert result[1]["category"] == "Transport"
     mock_file.assert_called_once_with("fake_path.csv", mode="r", encoding="utf-8")
+
+
+@patch("pandas.read_excel")
+def test_exel_reader(mock_read_excel):
+    mock_read_excel.return_value = pd.DataFrame([
+        {"date": "2024-01-01", "amount": "150", "category": "Shopping"},
+        {"date": "2024-01-03", "amount": "300", "category": "Rent"}
+    ])
+
+    result = exel_reader("fake_path.xlsx")
+    assert len(result) == 2
+    assert result[0]["date"] == "2024-01-01"
+    assert result[1]["amount"] == "300"
+    mock_read_excel.assert_called_once_with("fake_path.xlsx", dtype=str, engine='openpyxl')
